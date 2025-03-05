2fba9f1eaf46630b65d256a5d5dcfe590afa6f9d
Kseplex <Kseho163@gmail.com>
2025-03-05 12:39:11 +0400

    Todo fixes

--- src/test/java/pages/NavigationMenu.java
+++ src/test/java/pages/NavigationMenu.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class NavigationMenu {
+
     //TODO:здесь опиши локаторы для навигационного меню
     //TODO: неверное название метода
     //todo: в методе нет смысла, он не относится к навигационному меню, xpath соответствует всем элементам с текстом
--- src/test/java/steps/Task1Steps.java
+++ src/test/java/steps/Task1Steps.java
@@ -2,21 +2,16 @@ package steps;
 
 import com.codeborne.selenide.ElementsCollection;
 import org.junit.jupiter.api.Assertions;
-import pages.SelectMenuPage;
-
 import java.util.List;
 
-
-import static com.codeborne.selenide.Selenide.$$x;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 
 public class Task1Steps extends BaseSteps {
 
     private final String SEARCH_HEADER = "Select Menu";
     private final List<String> expectedSelectOptions = List.of("Group 1, option 1", "Group 1, option 2",
             "Group 2, option 1", "Group 2, option 2", "A root option", "Another root option");
-    private final SelectMenuPage selectMenuPage = new SelectMenuPage();
+    private final ElementsCollection collection = selectMenuPage.valueCollection;
 
     public void checkHeader() {
         navigationMenu.createXpath("Widgets").click();
@@ -29,6 +24,6 @@ public class Task1Steps extends BaseSteps {
 
     public void checkDropdownMenu () {
         selectMenuPage.selectValue.click();
-        assertThat(selectMenuPage.valueCollection.texts()).isEqualTo(expectedSelectOptions);
+        assertThat(collection.texts()).isEqualTo(expectedSelectOptions);
     }
 }
