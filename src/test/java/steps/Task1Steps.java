784c7de7126396384003934d4d7ff91c83c3b8fd
Kseplex <Kseho163@gmail.com>
2025-03-05 12:29:51 +0400

    Todo fixes

--- src/test/java/steps/Task1Steps.java
+++ src/test/java/steps/Task1Steps.java
@@ -6,13 +6,11 @@ import pages.SelectMenuPage;
 
 import java.util.List;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-
 public class Task1Steps extends BaseSteps {
     private final String SEARCH_HEADER = "Select Menu";
     private final List<String> expectedSelectOptions = List.of("Group 1, option 1", "Group 1, option 2",
             "Group 2, option 1", "Group 2, option 2", "A root option", "Another root option");
-    private final ElementsCollection collection = new SelectMenuPage().valueCollection;
+    ElementsCollection collection = selectMenuPage.valueCollection;
 
     public void checkHeader() {
         navigationMenu.createXpath("Widgets").click();
@@ -27,6 +25,6 @@ public class Task1Steps extends BaseSteps {
         selectMenuPage.selectValue.click();
         //TODO: не пиши xpath в методах, выноси на пейджи
         //TODO: xpath выглядит как ненадежный, попытайся привязаться к более уникальным атрибутам
-
+        //collection.texts();
     }
 }
